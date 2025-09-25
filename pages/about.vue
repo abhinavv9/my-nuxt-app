b8a785904df523ed15f823a29b8901ccc77a4640
Abhinav Verma <68147871+abhinavv9@users.noreply.github.com>
2025-09-25 10:32:20 +0530

    Add pages/about.vue

---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>About Us</h1>
+    <p>Learn more about our project. This is a simple demonstration of Nuxt.js capabilities.</p>
+  </div>
+</template>
+
+<script setup>
+// Composition API script for the about page
+</script>
+
+<style scoped>
+div {
+  padding: 2rem;
+}
+</style>

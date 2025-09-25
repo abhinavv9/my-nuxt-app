a71402bd90b35e5030bb23df6cdd9d44ba7c60aa
Abhinav Verma <68147871+abhinavv9@users.noreply.github.com>
2025-09-25 10:32:14 +0530

    Add app.vue

---
+++ app.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <nav>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/about">About</NuxtLink>
+      <NuxtLink to="/posts">Posts</NuxtLink>
+    </nav>
+    <hr>
+    <NuxtPage /> <!-- This is where your page content will be rendered -->
+  </div>
+</template>
+
+<style scoped>
+nav {
+  padding: 1rem;
+  background-color: #f0f0f0;
+}
+nav a {
+  margin-right: 1em;
+  text-decoration: none;
+  color: #333;
+}
+nav a:hover {
+  color: #007bff;
+}
+
+hr {
+  margin: 1rem 0;
+  border: 0;
+  height: 1px;
+  background: #eee;
+}
+</style>

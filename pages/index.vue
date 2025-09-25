042f0fe610b50a2bf0b2c90c808af2b540722d4c
Abhinav Verma <68147871+abhinavv9@users.noreply.github.com>
2025-09-25 10:32:17 +0530

    Add pages/index.vue

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Welcome to Nuxt.js!</h1>
+    <p>This is your homepage. Explore the site using the navigation links above.</p>
+  </div>
+</template>
+
+<script setup>
+// Composition API script for the homepage
+</script>
+
+<style scoped>
+div {
+  padding: 2rem;
+}
+</style>
